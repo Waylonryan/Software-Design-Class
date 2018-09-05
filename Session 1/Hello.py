bdac74f05744da323005fb49561d8ac226e4f8d6
Waylonryan <42824410+Waylonryan@users.noreply.github.com>
2018-09-05 00:13:41 -0400

    Session2 update

--- Session 1/Hello.py
+++ Session 1/Hello.py
@@ -1,8 +1,11 @@
-print("Hello, Waylon"
-print"Hello, Waylon"
-print("Hello, Waylon)
-print(Hello, Waylon)
-print(+2)
-print(2++2)
-print(02)
-print(5 68)
+# first_name = input("Input First Name:")
+# last_name = input("Input Last Name:")
+# print("hello,", first_name + " " + last_name)
+# print("Hello, {}".format("world"))
+# name = "Waylon"
+# print("hello, {}".format(name))
+# print('Congratulations, {:s}, you won {:d}th Academy award.'.format('Gary Oldman', 90))
+print("Pi equals {:.2f}".format(3.14159265))
+print("Growth rate: {:.2f}%".format(2.5))
+print("coordinates of Babson: {lat}, {lon}".format(lat="42.30N", lon="71.27W" ))
+
---
+++ Session 1/Helloo.py
@@ -0,0 +1,14 @@
+name = input()
+print(name)
+message = "Hello Im Waylon"
+print(message)
+a = "ABC"
+print(a)
+b = a
+a = "XYZ"
+print(a)
+print(b)
+first_name = "Waylon"
+last_name = "Ryan"
+name = first_name + ' ' +last_name
+
